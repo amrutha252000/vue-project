6e80207a3397328ea3af473d6913a3fcb975e6ce
Amrutha <amruthaanuanand@gmail.com>
2023-10-21 18:43:16 -0500

    code added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const Sport = require('../models/schema');
 
-/* GET home page. */
+/*const bodyParser  = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));*/
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/setposts', function(req, res, next){
-  res.json({msg:'post req is working'})
+  const newSport = new Sport({
+    sportdes: req.body.sportdes,
+    time_stamp: req.body.time_stamp,
+  });
+  newSport.save().then(val => {
+    res.json({ msg: "post req is working", val: val })
+  })
 });
 
 router.get('/getposts', function(req, res, next){
